d66ed56900330a524a366ae8ab8bddefb6d3a387
yantavares <yantdo1@gmail.com>
2023-05-25 09:35:24 -0300

    finished first gift

--- src/components/Presente/Presente.js
+++ src/components/Presente/Presente.js
@@ -2,10 +2,15 @@ import "./Presente.css";
 import { faLock, faLockOpen } from "@fortawesome/free-solid-svg-icons";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 
-function Presente({ date, shouldOpen, content }) {
+function Presente({ date, shouldOpen, picture, content }) {
   return (
     <>
-      <div className={shouldOpen ? "book" : "book-close"}>
+      <div className={shouldOpen ? "book align" : "book-close align"}>
+        <img
+          src={picture}
+          alt="totoro"
+          style={{ height: "100px", width: "100px", borderRadius: "20px" }}
+        />
         <p>{content}</p>
         <div className="cover">
           <p>{date}</p>
--- src/components/Presentes/Presentes.js
+++ src/components/Presentes/Presentes.js
@@ -1,14 +1,20 @@
 import { useState } from "react";
 import "./Presentes.css";
 import Presente from "../Presente/Presente";
+import totoro from "../../images/totoro.jpeg";
 
 function Presentes() {
   return (
     <div className="presentes-container container">
-      <Presente date={"14/14/14"} shouldOpen={true} content={"Nada"} />
-      <Presente date={"14/14/14"} shouldOpen={false} content={"Nada"} />
-      <Presente date={"14/14/14"} shouldOpen={false} content={"Nada"} />
-      <Presente date={"14/14/14"} shouldOpen={false} content={"Nada"} />
+      <Presente
+        date={"25/06/23"}
+        shouldOpen={true}
+        picture={totoro}
+        content={"Te amo meu bem, meu amor, minha NAMORADA!!!  "}
+      />
+      <Presente date={"Próximo date"} shouldOpen={false} content={"Nada"} />
+      <Presente date={"Daqui 2 dates"} shouldOpen={false} content={"Nada"} />
+      <Presente date={"Daqui 3 dates"} shouldOpen={false} content={"Nada"} />
     </div>
   );
 }
